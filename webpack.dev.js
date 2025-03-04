237327a97b21c79fd5efa21b2e5516445ca64292
simone-gasparini-94 <simone.gasparini.2@gmail.com>
2025-03-04 13:34:54 +0100

    Template complete

---
+++ webpack.dev.js
@@ -0,0 +1,13 @@
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+
+module.exports = merge(common, {
+    mode: "development",
+    devtool: "eval-source-map",
+    devServer: {
+        watchFiles: ["./src/template.html"],
+        port: 3000,
+        open: true,
+        hot: true,
+    },
+});
